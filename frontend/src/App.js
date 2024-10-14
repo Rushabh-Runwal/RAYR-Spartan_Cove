26d8cf36d9eb557c10561a09e50a361d4da598a8
Rushabh Runwal <rushabhrunwal@Rushabhs-MacBook-Pro.local>
2024-10-14 16:36:37 -0700

    feat: add protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
 // src/App.js
-import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Signup from './components/Signup';
-import ChatPage from './components/ChatPage';
+import React from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import SignUp from "./components/Signup";
+import ChatPage from "./components/ChatPage";
+
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = localStorage.getItem("userInfo") !== null;
+  return isAuthenticated ? children : <Navigate to="/" replace />;
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Signup />,
+    element: <SignUp />,
   },
   {
     path: "/chats",
-    element: <ChatPage />,
+    element: (
+      <ProtectedRoute>
+        <ChatPage />
+      </ProtectedRoute>
+    ),
   },
 ]);
 
 function App() {
-    return (
-        <div className="App">
-            <RouterProvider router={router} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
